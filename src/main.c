b473570e4312b6df1a3961578a1ff7f386098d6a
nrobinso <nrobinso@student.42.fr>
2024-04-15 13:25:00 +0200

    doubles checker working but needs to be cleaned up

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 08:08:40 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/04/14 23:10:32 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/04/15 13:21:59 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,93 @@ int is_args_numbers(char *str, int *nb_args)
 	return (1);
 }
 
+int	is_args_double(char *str, char *find)
+{
+	int i;
+	int j;
+	int count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	while (str && str[i])
+	{
+		while (str[i] == find[j])
+		{
+			i++;
+			j++;
+			if ((find[j] == '\0') && ((str[i] == ' ') || (str[i] == '\0')))
+			{
+				count++;
+				break ;
+			}
+		}
+		j = 0;
+		while (str[i] != ' ')
+			i++;
+		i++;
+	}
+	if (count == 1)
+		count = 0;
+	return (count);
+}
+
+char *str_to_find(char *str)
+{
+	int i;
+	int j;
+	char buffer[100];
+	char *return_str;
+
+	i = 0;
+	j = 0;
+	while (str[i] != ' ')
+	{
+		buffer[j] = str[i];
+		j++;
+		i++;
+	}
+	buffer[j] = '\0';
+	i++;
+	return_str = ft_strdup(buffer);
+	return (return_str);
+}
+
+
+
+char *cleanzero(char *str)
+{
+	int i;
+	int j;
+	char buffer[100];
+	char *ret_str;
+
+	i = 0;
+	j = 0;
+	while (str && str[i] == '0')
+		i++;
+	if (str[i] == ' ')
+		i--;
+	while (str && str[i])
+	{
+		if (str[i] == ' ' && str[i + 1] == '0')
+		{
+			i++;
+			while (str[i] == '0')
+				i++;
+			if ((str[i - 1] == '0' && str[i] == ' ' ) || str[i + 1] == '0' || str[i + 1] == '\0')
+				i--;
+			buffer[j] = ' ';
+			j++;	
+		}
+		buffer[j] = str[i];
+		i++;
+		j++;
+	}
+	buffer[j] = '\0';
+	ret_str = ft_strdup(buffer);	
+	return (ret_str);	
+}
 
 
 
@@ -81,31 +168,57 @@ int is_args_numbers(char *str, int *nb_args)
 int main(int argc, char *argv[])
 {
 	char *str;
-	char  **tab;
+	char *clean_zero;
+	char *findstr;
 	int	 nb_args;
+	int i;
 
+	i = 0;
 	if (argc < 2)
 		return (ft_putstr_fd("Error\n", 1),1);
 	nb_args = 0;
 	str = join_args(argc, argv);
 
-	if (!is_args_numbers(str, &nb_args))
+	printf("str : %s\n", str);
+
+	clean_zero = cleanzero(str); 
+	printf("str : %s\n", clean_zero);
+
+
+	
+
+
+	
+	
+	if (!is_args_numbers(clean_zero, &nb_args))
 	{
 		free(str);	
 		return (ft_putstr_fd("Error\n", 1),1);
 	}
 
-	tab = ft_split(str, ' ');
-
-
-
-
 	
-	printf("nb : %s\n", tab[nb_args - 1]);
+	findstr = str_to_find(&clean_zero[i]);
+	printf("%s\n", findstr);
+	if (is_args_double(clean_zero, findstr))
+		printf("double\n");
+	else
+		printf("ok no double\n");
+	
+	
+	
 
 
 
 
+
+
+
+//	tab = ft_split(str, ' ');
+//	printf("nb : %s\n", findstr);
+
+
+
+	free(findstr);
 	free(str);
 	return (0);
 }
