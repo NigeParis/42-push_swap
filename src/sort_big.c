ae6fe16729713d2d7c8cb11eea60a89ff4b3aefd
Nigel Robinson <nrobinso@paul-f4Ar7s9.clusters.42paris.fr>
2024-04-25 12:21:59 +0200

    radix sur 1 chiffre marche

--- src/sort_big.c
+++ src/sort_big.c
@@ -6,75 +6,72 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 22:17:02 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/04/25 00:58:33 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/04/25 12:21:12 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-// void    sort_big (t_stack **a, t_stack **b, int nb_elements)
-// {
-//     t_stack *tmp;
-//     //t_stack *begin;
-//     unsigned int i = 0;
-//     unsigned int nbr = 0;
 
-//     tmp = *a; 
-//     //begin = *a;
-//     ft_printf("------>nb_elements %d\n", nb_elements);
+void sort_big_nbr(t_stack **a, t_stack **b, int nb_elements, unsigned int index) 
+{
+    t_stack **tmp;
+    unsigned int i = 0;
+    unsigned int nbr = 0;
+    //unsigned int nb2 = 0;
+    tmp = a;
+    //print_stack(a, 'B');
+    if (!*a)
+        return ;
+    while (i < (unsigned int)nb_elements) {
+        while (tmp && i < (unsigned int)nb_elements) 
+        {
+            nbr = (*tmp)->id % 10;
+           // nb2 = (*tmp)->id % 100;
+         //   ft_printf("if %u here %u\n", index, (*tmp)->id % 10);
+            //ft_printf("num:%u %%%u\n",(*tmp)->id, nbr);
+
+            
+            if (nbr == index)
+            {
+                pb(tmp, b);
+                break;
+            }
+            else
+            {
+                if ((*tmp)->next != NULL)
+                {
+                    ra(a);
+                    tmp = &(*tmp)->next;
+                }  
+                break ;
+            }
+            
+        }
+        if ((*a))
+            tmp = &(*a); 
+        else
+            tmp = 0;
+        i++;
+    }
+}
 
 
-
-//     while (i < (unsigned int)nb_elements - 1)
-//     {
-    
-//         while (tmp)
-//         {
-        
-
-//             ft_printf("--%u----%u-->%u\n", tmp->id, i, (tmp->id %10));
-
-//             nbr = tmp->id %10;    
-//             if (nbr == 0)
-//                 pb(&tmp, b);
-    
-//             if (tmp->next == NULL)
-//                 break ;
-//             tmp = tmp->next;
-          
-//         }
-//         ft_printf("\n");
-//         (*a) = tmp; 
-//         //tmp = begin->next; 
-     
-//         i++;
-//     }
-// }
 
 
 void sort_big(t_stack **a, t_stack **b, int nb_elements) 
 {
-    t_stack *tmp = *a;
-    unsigned int i = 0;
-    unsigned int nbr = 0;
-    //unsigned int nb = 0;
-
-    while (i < (unsigned int)nb_elements) {
-        while (tmp) {
-            
-            if ((nbr == 0) && (tmp->id))
-            {
-                nbr = tmp->id % 10;
-                ft_printf("--%u-- %u--%u-->%u\n", tmp->id, nbr, i, (tmp->id %10));
-                pb(a, b);
-                tmp = (*a)->next;
-                nbr = tmp->id % 10;
-                break;
-            }
-            tmp = tmp->next;
-        }
-        tmp = (*a); 
-        i++;
-    }
+    sort_big_nbr(a, b, nb_elements, 0);
+    sort_big_nbr(a, b, nb_elements, 1);
+    sort_big_nbr(a, b, nb_elements, 2);
+    sort_big_nbr(a, b, nb_elements, 3);
+    sort_big_nbr(a, b, nb_elements, 4);
+    sort_big_nbr(a, b, nb_elements, 5);
+    sort_big_nbr(a, b, nb_elements, 6);
+    sort_big_nbr(a, b, nb_elements, 7);
+    sort_big_nbr(a, b, nb_elements, 8);
+    sort_big_nbr(a, b, nb_elements, 9);
+    
+   
 }
 
