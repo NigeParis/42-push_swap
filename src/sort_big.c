4252bc18065ddc6340693594c99ff1585bc593d4
Nigel Robinson <nrobinso@paul-f4Br3s2.clusters.42paris.fr>
2024-05-03 09:57:05 +0200

    sort testing

--- src/sort_big.c
+++ src/sort_big.c
@@ -6,16 +6,18 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 22:17:02 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/05/03 08:20:29 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/05/03 09:55:32 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-// void sort_big_nbr(t_stack **a, t_stack **b, int nb_nodes) 
+// void sort(t_stack **a, t_stack **b, int nb_nodes, int multiply) 
 // {
 //     t_stack *tmp;
-//     (void) b;
+// 	unsigned int nbr;
+// 	unsigned int j = 0;
+// 	int y;
 
 //     unsigned int i = 0;
 
@@ -24,61 +26,63 @@
 //     if (!tmp)
 //         return;
 
-//     while (i < 1) 
+//     while (i < 10) 
 //     {
-
-//         while (tmp != NULL)
+// 		y = 0;
+//         while ((y < nb_nodes) && (*a))
 //         {
-//             ft_printf("i: %u val: %u nbr_ele: %d\n", i, tmp->valeur, nb_nodes);
-            
-//             if (!(1 << tmp->id))
-//                 pb(a,b);
-//             else
-//                 ra(a);
-            
+//         	tmp = *a;
 
-//             if (tmp->next != NULL)
-//                 tmp = tmp->next;
-//             else
-//                 break;
+//             nbr = (tmp->id %(10 * multiply)) / multiply;
+//             if (j == nbr)
+//             	pb(a,b);
+// 			else
+//                 ra(a);
+// 			y++;
 //         }
-        
-//         tmp = *a;
+// 		if (j == 9)
+// 		{
+// 			rra(a);
+// 			j = 0;
+// 		}
+// 		else
+//         	j++;
 //         i++;
-//         ft_printf("\n");
-//     }
+		
+//     }	
+	
 // }
 
 
 
-void	sort(t_stack **a, t_stack **b, unsigned int nodes, unsigned int mask)
-{
-	t_stack	*tmp;
-	unsigned int    bit_pos;
-	unsigned int	node_pos;
-	unsigned int	size_in_bits;
+// void	sort(t_stack **a, t_stack **b, unsigned int nodes, unsigned int mask)
+// {
+// 	t_stack	*tmp;
+// 	unsigned int    bit_pos;
+// 	unsigned int	node_pos;
+// 	unsigned int	size_in_bits;
 	
-	bit_pos = 0;
-	tmp = *a;
-	nodes = ft_lstsizenode(tmp);
-	size_in_bits = sizeof(unsigned int) * 8;
-	while (bit_pos < size_in_bits)
-	{
-		node_pos = 0;
-		while (node_pos < nodes)
-		{
-		    tmp = *a;
-			if (((tmp->id >> bit_pos) & mask) == 1)
-				ra(a);
-			else
-				pb(a, b);
-            node_pos++;
-		}
-		while (ft_lstsizenode(*b) != 0)
-			pa(a, b);
-		bit_pos++;
-	}
-}
+// 	bit_pos = 0;
+// 	tmp = *a;
+// 	nodes = ft_lstsizenode(tmp);
+// 	size_in_bits = sizeof(unsigned int) * 8;
+// 	while (bit_pos < size_in_bits)
+// 	{
+// 		node_pos = 0;
+// 		while (node_pos < nodes)
+// 		{
+// 		    tmp = *a;
+// 			if (((tmp->id >> bit_pos) & mask) == 1)
+// 				ra(a);
+// 			else
+// 				pb(a, b);
+//             node_pos++;
+// 		}
+// 		while (ft_lstsizenode(*b) != 0)
+// 			pa(a, b);
+// 		bit_pos++;
+// 	}
+// }
 
 
 
@@ -88,7 +92,14 @@ void sort_big(t_stack **a, t_stack **b, unsigned int stack_size)
     int nb_nodes = stack_size - 1;
 
 
-    sort(a, b, nb_nodes, 0b00000001);
+    sort(a, b, nb_nodes, 1);
+	while ((*b))
+		pa(a,b);
+    sort(a, b, nb_nodes, 10);
+	while ((*b))
+		pa(a,b);
+    sort(a, b, nb_nodes, 100);
+    // sort(a, b, nb_nodes, 0b00000001);
 	print_stack(a, 'A');
 	print_stack(b, 'B');
 
