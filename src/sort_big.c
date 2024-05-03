80efbd6159ad6e7cceab642ba18f2db12abcae37
Nigel Robinson <nrobinso@paul-f4Br3s2.clusters.42paris.fr>
2024-05-03 08:21:27 +0200

    intehrated mask in function call

--- src/sort_big.c
+++ src/sort_big.c
@@ -6,13 +6,13 @@
 /*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 22:17:02 by nrobinso          #+#    #+#             */
-/*   Updated: 2024/05/02 21:27:45 by nrobinso         ###   ########.fr       */
+/*   Updated: 2024/05/03 08:20:29 by nrobinso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/push_swap.h"
 
-// void sort_big_nbr(t_stack **a, t_stack **b, int nb_elements) 
+// void sort_big_nbr(t_stack **a, t_stack **b, int nb_nodes) 
 // {
 //     t_stack *tmp;
 //     (void) b;
@@ -29,7 +29,7 @@
 
 //         while (tmp != NULL)
 //         {
-//             ft_printf("i: %u val: %u nbr_ele: %d\n", i, tmp->valeur, nb_elements);
+//             ft_printf("i: %u val: %u nbr_ele: %d\n", i, tmp->valeur, nb_nodes);
             
 //             if (!(1 << tmp->id))
 //                 pb(a,b);
@@ -51,24 +51,24 @@
 
 
 
-void	sort_big_nbr(t_stack **a, t_stack **b, unsigned int nb_elements)
+void	sort(t_stack **a, t_stack **b, unsigned int nodes, unsigned int mask)
 {
 	t_stack	*tmp;
 	unsigned int    bit_pos;
 	unsigned int	node_pos;
 	unsigned int	size_in_bits;
-
+	
 	bit_pos = 0;
 	tmp = *a;
-	nb_elements = ft_lstsizenode(tmp);
+	nodes = ft_lstsizenode(tmp);
 	size_in_bits = sizeof(unsigned int) * 8;
 	while (bit_pos < size_in_bits)
 	{
 		node_pos = 0;
-		while (node_pos < nb_elements)
+		while (node_pos < nodes)
 		{
 		    tmp = *a;
-			if (((tmp->id >> bit_pos) & 1) == 1)
+			if (((tmp->id >> bit_pos) & mask) == 1)
 				ra(a);
 			else
 				pb(a, b);
@@ -85,12 +85,12 @@ void	sort_big_nbr(t_stack **a, t_stack **b, unsigned int nb_elements)
 
 void sort_big(t_stack **a, t_stack **b, unsigned int stack_size) 
 {
-    int nb_elements = stack_size - 1;
+    int nb_nodes = stack_size - 1;
 
 
-    sort_big_nbr(a, b, nb_elements);
-	// print_stack(a, 'A');
-	// print_stack(b, 'B');
+    sort(a, b, nb_nodes, 0b00000001);
+	print_stack(a, 'A');
+	print_stack(b, 'B');
 
 
 }
